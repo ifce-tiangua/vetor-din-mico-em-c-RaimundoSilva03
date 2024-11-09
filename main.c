4a01a8335364616186b712172eb8a4bd179b8889
RaimundoSilva03 <126274796+RaimundoSilva03@users.noreply.github.com>
2024-11-08 21:33:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+int main()
+{
+    int tam;
+    int *vet = (int*)malloc(tam*sizeof(int));
+    scanf("%d", &tam);
+    if(vet==NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
+    for(int i = 0; i < tam; i++) {
+        scanf("%d", &vet[i]);
+    }
+    printf("[");
+    for(int i = 0; i < tam; i++) {
+        printf("%d", vet[i]);
+        if(i<tam-1)
+        printf(",");
+    }
+    printf("]");
     return 0;
 }
